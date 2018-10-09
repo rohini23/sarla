0d41d5fbd09477a2ee0556d52bafa7214a143bbf
Rohini <kuperkarrohini@gmail.com>
2018-10-09 13:10:42 +0530

    changes to viewport

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,19 @@
-// var nav = document.querySelector(".navbar");
-// var sticky = nav.offsetTop;
-// function myFunction() {
-//   if (window.pageYOffset >= sticky) {
-//     nav.classList.add("sticky");
-//   } else {
-//     nav.classList.remove("sticky");
-//   }
-// }
-// window.onscroll = function() {
-//   myFunction();
-// };
-
-// var a = document.querySelector(".deactive>a");
-// document.querySelector(".deactive").addEventListener("click", function() {
-//   console.log("---------");
-//   a.classList.remove("deactive");
-//   a.classList.add("active");
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+  if ($navbarBurgers.length > 0) {
+    $navbarBurgers.forEach(el => {
+      el.addEventListener("click", () => {
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+        el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
 
 var header = document.querySelector(".prod-list-ul");
 console.log("header = ", header);
@@ -30,4 +27,48 @@ for (var i = 0; i < links.length; i++) {
     current[0].className = current[0].className.replace(" active", "");
     this.className += " active";
   });
+}
+var current = document.querySelector(".prod-item.active");
+window.addEventListener("keydown", function(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+  if ([40, 38].includes(key)) e.preventDefault();
+});
+window.addEventListener("keyup", changeSelectedProduct);
+
+document
+  .querySelector(".prod-list-ul li:first-child")
+  .addEventListener("click", function() {
+    changeSelectedProduct({ keyCode: 38 });
+  });
+
+document
+  .querySelector(".prod-list-ul li:last-child")
+  .addEventListener("click", function() {
+    changeSelectedProduct({ keyCode: 40 });
+  });
+
+function changeSelectedProduct(e) {
+  var key = e.keyCode ? e.keyCode : e.which;
+  if (key === 40) {
+    var temp = current && current.nextElementSibling;
+    if (!temp) return;
+    if (temp.classList.contains("prod-item")) {
+      current.classList.remove("active");
+      current.classList.add("deactive");
+      current = temp;
+      current.classList.add("active");
+      current.classList.remove("deactive");
+      // current.scrollIntoView({ behavior: "smooth", block: "center" });
+    }
+  }
+  if (key === 38) {
+    var temp = current && current.previousElementSibling;
+    if (!temp) return;
+    current.classList.remove("active");
+    current.classList.add("deactive");
+    current = temp;
+    current.classList.add("active");
+    current.classList.remove("deactive");
+    // current.scrollIntoView({ behavior: "smooth", block: "center" });
+  }
 }
